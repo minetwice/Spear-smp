965d49ffcf3d405302c48cc2071172eeaebdd30a
minetwice <ytd82774@gmail.com>
2025-10-17 11:58:57 +0530

    Add ActionBarUtils for sending action bar messages

---
+++ src/main/java/me/minetwice/elementalspears/utils/ActionBarUtils.java
@@ -0,0 +1,9 @@
+package me.minetwice.elementalspears.utils;
+
+import org.bukkit.entity.Player;
+
+public class ActionBarUtils {
+    public static void sendActionBar(Player player, String msg) {
+        player.sendActionBar(msg);
+    }
+}
