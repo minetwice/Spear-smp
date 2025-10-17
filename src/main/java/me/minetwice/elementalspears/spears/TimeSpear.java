625127e01aaf42b10fcf47318e9ef303e1764c3a
minetwice <ytd82774@gmail.com>
2025-10-17 11:50:31 +0530

    Add TimeSpear class with slowing ability

---
+++ src/main/java/me/minetwice/elementalspears/spears/TimeSpear.java
@@ -0,0 +1,23 @@
+package me.minetwice.elementalspears.spears;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class TimeSpear extends ElementalSpear {
+    @Override
+    public String getName() { return "Time Spear"; }
+    @Override
+    public Material getMaterial() { return Material.TRIDENT; }
+    @Override
+    public void onAttack(Player player, Entity victim) {
+        if (victim instanceof Player target) {
+            target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * 5, 2));
+            player.sendMessage("§bTime slowed for 5s!");
+        }
+    }
+    @Override
+    public String abilityName() { return "Slow enemy for 5s"; }
+}
