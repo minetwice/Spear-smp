79a82094de4d74778717f395f63d322501bdb089
minetwice <ytd82774@gmail.com>
2025-10-17 12:37:26 +0530

    Implement poison effect on attack for PoisonSpear
    Added poison effect to target players for 7 seconds on attack.

--- src/main/java/me/minetwice/elementalspears/spears/PoisonSpear.java
+++ src/main/java/me/minetwice/elementalspears/spears/PoisonSpear.java
@@ -10,13 +10,19 @@ import org.bukkit.Particle;
 public class PoisonSpear extends ElementalSpear {
     @Override
     public String getName() { return "Poison Spear"; }
+
     @Override
     public Material getMaterial() { return Material.TRIDENT; }
-    @));
+
+    @Override
+    public void onAttack(Player player, Entity victim) {
+        if (victim instanceof Player target) {
+            target.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 20 * 7, 1));
         }
         player.getWorld().spawnParticle(Particle.SPELL_MOB, victim.getLocation(), 15, 0.3, 1, 0.3, 1);
         player.sendMessage("§2Poisoned for 7s!");
     }
+
     @Override
     public String abilityName() { return "Poison enemy for 7s"; }
 }
