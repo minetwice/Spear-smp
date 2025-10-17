1fa6d13728288535d4ce0a88f2e62348b7734928
minetwice <ytd82774@gmail.com>
2025-10-17 11:40:24 +0530

    Set up ElementalSpears plugin structure
    Initialize the ElementalSpears plugin and register commands and event listeners.

--- src/main/java/me/minetwice/elementalspears/ElementalSpears.java
+++ src/main/java/me/minetwice/elementalspears/ElementalSpears.java
@@ -1,1 +1,26 @@
+package me.minetwice.elementalspears;
 
+import me.minetwice.elementalspears.commands.AdminCommands;
+import me.minetwice.elementalspears.commands.SpearCommands;
+import me.minetwice.elementalspears.listeners.PlayerJoinListener;
+import me.minetwice.elementalspears.listeners.SpearListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElementalSpears extends JavaPlugin {
+    private static ElementalSpears instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(new SpearListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+
+        getCommand("getspear").setExecutor(new SpearCommands());
+        getCommand("clearcords").setExecutor(new AdminCommands());
+        getCommand("clearspear").setExecutor(new AdminCommands());
+    }
+
+    public static ElementalSpears getInstance() { return instance; }
+}
