f2664a41d107feca720e51822782b8ca166bef8b
minetwice <ytd82774@gmail.com>
2025-10-17 13:02:30 +0530

    Update PoisonSpear.java

--- src/main/java/me/minetwice/elementalspears/spears/PoisonSpear.java
+++ src/main/java/me/minetwice/elementalspears/spears/PoisonSpear.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.Particle;
+import org.bukkit.Color;
 
 public class PoisonSpear extends ElementalSpear {
     @Override
@@ -17,7 +18,9 @@ public class PoisonSpear extends ElementalSpear {
         if (victim instanceof Player target) {
             target.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 20 * 7, 1));
         }
-        player.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, victim.getLocation(), 15, 0.3, 1, 0.3, 1);
+        // Green poison particles for 1.21.1
+        Particle.DustOptions dust = new Particle.DustOptions(Color.fromRGB(0, 255, 0), 1.0f);
+        player.getWorld().spawnParticle(Particle.DUST, victim.getLocation(), 15, 0.3, 1, 0.3, 0, dust);
         player.sendMessage("§2Poisoned for 7s!");
     }
     @Override
